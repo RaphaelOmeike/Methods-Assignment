acf766754c11afdce529aea9a0bba21d52c8c770
RaphaelOmeike <109993209+RaphaelOmeike@users.noreply.github.com>
2022-08-09 20:53:42 +0100

    Add files via upload

---
+++ Chapter 10 - Recursion/Question 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Question_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Number,N");
+            int n = int.Parse(Console.ReadLine());
+            Loops(n - 1);
+        }
+        public static void Loops(int k)
+        {
+            for(int i = 1; i <= k; i++)
+            {
+                Loops(k - 1);
+            }
+            if(k < 0)
+            {
+                return;
+            }
+        }
+    }
+}
---
+++ Chapter 10 - Recursion/Question 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Number,N");
+            int n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter The Number,K");
+            int k1 = int.Parse(Console.ReadLine());
+            PrintSequence(n1,k1);
+        }
+        static void PrintSequence(int n, int t)
+        {
+            if(n < 1)
+            {
+                return;
+            }
+            for(int i = 1; i <= t; i++)
+            {
+                Console.WriteLine(t-(n-1) + "," + i);
+            }
+            PrintSequence(n-1,t - 1);
+        }
+    }
+}
